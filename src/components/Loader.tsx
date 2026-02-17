1182684c917cc2023e62bd7b72620f0e0be3ed76
[dyad] <arun.baikan@realvariable.com>
2026-02-17 14:48:44 +0530

    Reverted all changes back to version f7c916125693635722ac1d8f1f8fa821f520d22b

--- src/components/Loader.tsx
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import { Loader2 } from 'lucide-react';
-
-const Loader = ({ size = 24 }: { size?: number }) => {
-  return <Loader2 className="animate-spin text-primary" size={size} />;
-};
-
-export default Loader;
